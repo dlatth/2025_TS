a2bd643082ecea728a90252b35e13ba6f9c61aba
Soyeon Lim <156628006+dlatth@users.noreply.github.com>
2025-04-02 15:14:10 +0900

    🎉 init : Initial commit

--- readme.md
+++
@@ -1,8 +0,0 @@
-# 실행방법
-
-1. docker compose up -d
-2. docker compose exec ts /bin/bash
-3. npm install
-4. npm run build
-5. npm run start
-6. 브라우저로 localhost:9000 접속
--- ts_basic/src/index.ts
+++ ts_basic/src/index.ts
@@ -1,4 +1,4 @@
-import World from "./world"; // webpack.config.js파일의 resolve 설정에 따라 ts 생략 가능
-const root: HTMLElement | null = document.getElementById("root");
-const world = new World("Hello World!!!");
+import World from './world'; // webpack.config.js파일의 resolve 설정에 따라 ts 생략 가능
+const root: HTMLElement | null = document.getElementById('root');
+const world = new World('Hello World!!!');
 world.sayHello(root);
