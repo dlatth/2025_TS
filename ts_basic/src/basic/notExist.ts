4907d51612312f1e20a1cef7bd9956c4d7d74fe2
Soyeon Lim <156628006+dlatth@users.noreply.github.com>
2025-04-09 14:16:28 +0900

    🧪 test: not exist type test 2

--- ts_basic/src/basic/notExist.ts
+++ ts_basic/src/basic/notExist.ts
@@ -2,10 +2,18 @@ export default function notExistSample() {
   const name = null;
   console.log('notExistSample 1: ', typeof name, name);
 
-  // name = 'JIT'; // 주석을 빼고도 테스트 실행
   if (name) {
     console.log('notExistSample 2: ', typeof name, 'my name is ' + name);
   } else {
     console.log('notExistSample 3: ', 'my name is ' + name);
+  }
+
+  const age = undefined;
+  console.log('notExist sample 4: ', typeof age, age);
+
+  if (age) {
+    console.log('notExist sample 5: ', 'my age is ' + age + ' years old.');
+  } else {
+    console.log('notExist sample 6: ', 'my age is not defined');
   }
 }
