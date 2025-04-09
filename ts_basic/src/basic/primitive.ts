cfb248c9c8f489465d4a3349d7b61f51078d5a1f
Soyeon Lim <156628006+dlatth@users.noreply.github.com>
2025-04-09 11:34:23 +0900

    🧪 test: primitive type test2, 3

--- ts_basic/src/basic/primitive.ts
+++ ts_basic/src/basic/primitive.ts
@@ -1,5 +1,16 @@
 export default function primitiveSample() {
   console.log('YJU');
   const name: string = 'JIT'; // 형추론으로 인해 let name = 'JIT'으로도 가능
+  // name = 123 // 타입 체크로 에러 발생
   console.log('primitive sample 1 : ', typeof name, name);
+
+  const age: number = 20;
+  console.log('primitive sample 2 : ', typeof age, age);
+
+  const isSingle: boolean = true;
+  // isSingle  = 'yes'
+  console.log('primitive sample 3 : ', typeof isSingle, isSingle);
+
+  const isOver20: boolean = age >= 20;
+  console.log('primitive sample 4 : ', typeof isOver20, isOver20);
 }
