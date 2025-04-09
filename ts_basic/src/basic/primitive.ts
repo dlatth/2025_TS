a3585ec1868d1616c625282878011ce6394644e3
Soyeon Lim <156628006+dlatth@users.noreply.github.com>
2025-04-09 11:31:56 +0900

    🧪 test: primitive type test1

--- ts_basic/src/basic/primitive.ts
+++ ts_basic/src/basic/primitive.ts
@@ -1,3 +1,5 @@
 export default function primitiveSample() {
   console.log('YJU');
+  const name: string = 'JIT'; // 형추론으로 인해 let name = 'JIT'으로도 가능
+  console.log('primitive sample 1 : ', typeof name, name);
 }
